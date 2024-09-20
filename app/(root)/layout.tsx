1374b8fafa5f197917ec76a33247dd9a8068b8b8
OukhellouFst <oukhellou.fst@gmail.com>
2024-09-20 22:40:33 +0100

    Implement Next.js Route Groups and nested layouts
    - Delete pages.tsx
    - Create (root) folder:
      * Add page.tsx with simple component
      * Add layout.tsx with sidebar and children structure
    - Create (auth) folder:
      * Add layout.tsx and page.tsx
      * Add sign-in and sign-up subfolders with respective pages
    - Implement Next.js Route Groups for shared layouts
    - Set up file-based routing structure
    
    This commit establishes the basic routing and layout structure for the application,
    utilizing Next.js Route Groups for efficient shared layouts across multiple pages.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import {Inter,IBM_Plex_Serif}from "next/font/google"
-import "./globals.css";
 
 // Adding inter and ibmPlexSerif fonts with their corresponding parameters.
 const inter = Inter({subsets:["latin"],variable:'--font-inter'})
@@ -10,17 +8,6 @@ const ibmPlexSerif = IBM_Plex_Serif({
   weight: ['400','700'],
   variable: '--font-ibm-plex-serif'
 })
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 // chaning the default metadata
 export const metadata: Metadata = {

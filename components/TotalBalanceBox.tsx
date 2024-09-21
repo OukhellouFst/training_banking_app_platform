d709ea70cf8b3d633e08bcd918ca9ebcbcb8a57a
OukhellouFst <oukhellou.fst@gmail.com>
2024-09-21 17:01:47 +0100

    Add doughnut chart to TotalBalance component
    - Install chart.js and react-chartjs-2
    - Create DoughnutChart component with default data
    - Integrate DoughnutChart into TotalBalance component

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = (
     {accounts=[],totalBanks,totalCurrentBalance} : TotlaBalanceBoxProps
@@ -7,17 +8,19 @@ const TotalBalanceBox = (
   return (
     <section className='total-balance'>
         <div className='total-balance-chart'>
-            {/* Doughnut chart */}
+            <DoughnutChart 
+                accounts={accounts}
+            />
         </div>
         <div className='flex flex-col gap-6'>
             <h2 className='header-h2'>Bank Accounts: {totalBanks}</h2>
             <div className='flex flex-col gap-2'>
                 <p className='total-blance-label'>Total current balance</p>
-                <p className='total-balance-amount flex-center gap-2'>
+                <div className='total-balance-amount flex-center gap-2'>
                     <AnimatedCounter
                         amount={totalCurrentBalance}
                     />
-                </p>
+                </div>
             </div>
         </div>
     </section>
